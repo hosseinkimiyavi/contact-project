7f352162673bf0a9268fc6179d865ace9c900d5e
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2024-04-17 11:35:17 +0330

    add header file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-
+import Header from './Header'
 
 function App() {
 
 
   return (
     <>
-         <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Header/>
     </>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header
+() {
+  return (
+    <>
+
+    <h1 className='m-auto w-1/6 text-4xl font-bold mt-12 text-blue-700 pl-6'>Contact App</h1>
+
+    <h4 className='m-auto w-48 mt-4 bg-sky-200 rounded-lg pl-6 text-blue-700 '>First React.Js project</h4>
+    
+    
+    
+    
+    </>
+  )
+}

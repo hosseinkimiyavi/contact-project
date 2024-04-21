ccc2f59356cf2b57c4e186876286380cbe0ceb05
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2024-04-21 16:54:47 +0330

    tmiz krdn cod ha bakhs inputs

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,56 +1,99 @@
 import React, { useState } from "react";
 import ContactList from "./ContactList";
+import inputs from "./constans/inputs";
 
 export default function Form() {
-  const [contacts,setcontacts]=useState([])
-  const [alert,setalert]= useState("")
-  const [contact,setcontact] = useState({
-    name:"",lastname:"",email:"",phone:""
-   })
+  const [contacts, setcontacts] = useState([]);
+  const [alert, setalert] = useState("");
+  const [contact, setcontact] = useState({
+    name: "",
+    lastname: "",
+    email: "",
+    phone: "",
+  });
 
-   const changeHandler = (event)=>{
-    const name=event.target.name;
-    const value = event.target.value
+  const changeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
     setcontact({
-      ...contact,[name]:value
-    })
-
-
-   }
-   const subHandler = (event)=>{
+      ...contact,
+      [name]: value,
+    });
+  };
+  const subHandler = (event) => {
     event.preventDefault();
-    if (!contact.name || !contact.lastname || !contact.email || !contact.phone){
-      setalert("please enter valid data!")
+    if (
+      !contact.name ||
+      !contact.lastname ||
+      !contact.email ||
+      !contact.phone
+    ) {
+      setalert("please enter valid data!");
 
-      return
+      return;
     }
     setalert("");
 
-
-    setcontacts(contacts =>[...contacts,contact]);
+    setcontacts((contacts) => [...contacts, contact]);
     console.log(contacts);
     setcontact({
-      name:"",lastname:"",email:"",phone:""
-     });
-   
-   }
-
-
-
+      name: "",
+      lastname: "",
+      email: "",
+      phone: "",
+    });
+  };
 
   return (
     <>
-      <form action="" className="grid grid-cols-2 gap-6 border-4 shadow-lg shadow-blue-950 border-blue-700 rounded-xl px-6 border h-72 mt-6 items-center justify-center w-3/4 m-auto bg-cyan-100">
-        <input className="h-8 w3/4 font-semibold " value={contact.name}  type="text" name="name" placeholder="Name" onChange={changeHandler} />
-        <input className="h-8 w3/4 font-semibold " value={contact.lastname} type="text" name="lastname" placeholder="Last Name" onChange={changeHandler} />
+      <form
+        action=""
+        className="grid grid-cols-2 gap-6 border-4 shadow-lg shadow-blue-950 border-blue-700 rounded-xl px-6 border h-72 mt-6 items-center justify-center w-3/4 m-auto bg-cyan-100"
+      >
+        {inputs.map((input, index) =>(<input className={inputs.className} value={contact[input.name]} type={input.type} name={input.name} placeholder={input.placeholder} onChange={changeHandler} key={index} />))}
+        {/* <input
+          className="h-8 w3/4 font-semibold "
+          value={contact.name}
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={changeHandler}
+        />
+        <input
+          className="h-8 w3/4 font-semibold "
+          value={contact.lastname}
+          type="text"
+          name="lastname"
+          placeholder="Last Name"
+          onChange={changeHandler}
+        />
 
-        <input className="h-8 w3/4 font-semibold " value={contact.email} name="email" type="email" placeholder="Email" onChange={changeHandler} />
-        <input className="h-8 w3/4 font-semibold " value={contact.phone} type="number" name="phone" id="" placeholder="Phone" onChange={changeHandler} />
+        <input
+          className="h-8 w3/4 font-semibold "
+          value={contact.email}
+          name="email"
+          type="email"
+          placeholder="Email"
+          onChange={changeHandler}
+        />
+        <input
+          className="h-8 w3/4 font-semibold "
+          value={contact.phone}
+          type="number"
+          name="phone"
+          id=""
+          placeholder="Phone"
+          onChange={changeHandler}
+        /> */}
 
-
-        <button className="w-full h-8 border bg-blue-900 rounded-2xl  m-auto text-white mx-72 hover:bg-blue-800 " onClick={subHandler}>Add Contact</button>
-      </form> 
-      <div>{setalert && <p>{alert}</p> }</div>
+        <button
+          className="w-full h-8 border bg-blue-900 rounded-2xl  m-auto text-white mx-72 hover:bg-blue-800 "
+          onClick={subHandler}
+        >
+          Add Contact
+        </button>
+      </form>
+      <div>{setalert && <p>{alert}</p>}</div>
       <ContactList contacts={contacts} />
     </>
   );
---
+++ src/constans/inputs.js
@@ -0,0 +1,7 @@
+const inputs = [
+    { className: "h-8 w3/4 font-semibold", name: "name",type: "text",placeholder: "Name"},
+    { className: "h-8 w3/4 font-semibold", name: "lastname",type: "text", placeholder: "Last Name" },
+    { className: "h-8 w3/4 font-semibold", name: "email", type: "email", placeholder: "Email" },
+    { className: "h-8 w3/4 font-semibold", name: "phone", type: "number", placeholder: "Phone" },
+  ];
+ export default inputs  
